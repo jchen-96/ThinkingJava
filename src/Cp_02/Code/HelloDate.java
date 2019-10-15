a4a52262d9f820356afc6fc878032c63962093a4
jchen <1443616037@qq.com>
2019-10-15 11:04:28 +0800

    daily commit

---
+++ src/Cp_02/Code/HelloDate.java
@@ -0,0 +1,11 @@
+package Cp_02.Code;
+
+import java.util.*;
+
+class HelloDate{
+    public static void main(String[] args) {
+        // out 是一个静态的printStream对象
+        System.out.println("hello it's:");
+        System.out.println(new Date());
+    }
+}
